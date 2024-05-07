a8e955743629204d0d3655d09e5afea628f007f1
Rupesh Sahu <rupeshsahu@192.168.1.4>
2024-05-07 22:49:48 +0530

    algo files

---
+++ Array/basic.java
@@ -0,0 +1,43 @@
+package Array;
+import java.util.*;
+
+public class basic {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    
+    // Q: store a roll number 
+
+    //int a = 19;
+
+
+    // store person name 
+
+    //String name =  "rupesh sahu ";
+
+    // q ; store 5 roll no 
+
+    // int roll1 = 23;
+    // int roll2 = 24;
+    // int roll3 = 25;
+    // int roll4 = 26;
+    // System.out.println(roll1);
+
+
+
+    // store 5 roll no 
+
+    //int[] roll = new int[5];
+
+
+    // 0r directly 
+    int[] roll2 = {2,3,4,5,6};
+    System.out.println(0);
+
+
+    
+}
+}
